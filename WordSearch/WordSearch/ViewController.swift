2855fdf8467bbc8382990ae9a8fcc889232371f1
Ben Russell <mountainbrussells@gmail.com>
2017-05-25 09:54:49 -0600

    Initial Commit

---
+++ WordSearch/WordSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordSearch
+//
+//  Created by BenRussell on 5/25/17.
+//  Copyright © 2017 BenRussell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
